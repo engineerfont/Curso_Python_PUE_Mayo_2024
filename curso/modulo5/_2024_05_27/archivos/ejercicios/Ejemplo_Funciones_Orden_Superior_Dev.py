20744f976dd873de112399abfb96be853143d1fc
Xoan <xgp.pruebas@gmail.com>
2024-05-29 22:21:36 +0200

    initial command

---
+++ curso/modulo5/_2024_05_27/archivos/ejercicios/Ejemplo_Funciones_Orden_Superior_Dev.py
@@ -0,0 +1,16 @@
+personas = {}
+personas['id'] = int
+personas['altura'] = float
+
+
+entero = personas['id']
+doble = personas['altura']
+
+numero = input('Numero: ')
+
+ente = entero(numero)
+real = doble(numero)
+
+print(ente)
+print(real)
+
